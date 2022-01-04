f721155a709bc7980ab89a368373e809e28f84a8
Henry Derbyshire <henry@htderbyshire.com>
2022-01-04 07:26:24 +0000

    inital commit

---
+++ src/Disemvowel Trolls/Troll.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Troll {
+    public static void main(String[] args) {
+        System.out.println(disemvowel("This website is for losers LOL!"));
+        System.out.println(disemvowel("No offense but,\nYour writing is among the worst I've ever read"));
+        System.out.println(disemvowel("What are you, a communist?"));
+
+        System.out.println(disemvowelV2("This website is for losers LOL!"));
+    }
+
+    public static String disemvowel(String str) {
+        return Arrays.stream(str.split(""))
+                .filter(letter -> letter.matches("(?i)[^aeiou]"))
+                .collect(Collectors.joining(""));
+    }
+
+    public static String disemvowelV2(String str) {
+        return str.replaceAll("(?i)[aeiou]", "");
+    }
+}
